99d4c9315ed2f8f1e0a3836abd5d6c64d844f05d
Przemysław Paziewski <ppaazz56@gmail.com>
2021-10-06 16:32:50 +0200

    Directory fix v2

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import LandingPage from "./sections/landingPage/LandingPage.js";
 import { StyledEngineProvider } from "@mui/material/styles";
 import AboutMe from "./sections/aboutMe/AboutMe";
-import HideAppBar from "./appBar/AppBar.js";
+import HideAppBar from "./appbar/AppBar.js";
 import Skills from "./sections/skills/Skills";
 import Projects from "./sections/projects/Projects";
 import Contact from "./sections/contact/Contact";

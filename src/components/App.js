8f7f86310587285f8e0d1b3b96bebca75c1c0982
Przemysław Paziewski <ppaazz56@gmail.com>
2021-10-07 14:45:05 +0200

    few performance fixes

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Skills from "./sections/skills/Skills";
 import Projects from "./sections/projects/Projects";
 import Contact from "./sections/contact/Contact";
 
+
 export default function App() {
   return (
     <StyledEngineProvider injectFirst>
--- src/components/sections/projects/itemData.js
+++ src/components/sections/projects/itemData.js
@@ -1,40 +1,40 @@
 const itemData = [
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Breakfast",
     description: 'We eat this every morning'  
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
     description: 'We eat this every morning'
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
   },
   {
-    img: "https://res.cloudinary.com/nucat/image/upload/v1633183926/sample.webp",
+    img: "https://res.cloudinary.com/nucat/image/upload/c_scale,h_185,w_269/v1633183926/sample.webp",
     title: "Burger",
   },
   
